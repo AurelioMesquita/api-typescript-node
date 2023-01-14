02ef1f41f67bb22bfc0b7fc66e2dd48fe6d14260
Aurélio Nogueira Mesquita <aurelio.mesquita@interaduaneira.com.br>
2023-01-14 15:55:16 -0300

    Montando estrutura de rotas

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import express from "express";
+const { v4: uuidv4 } = require('uuid')
 
 const server = express();
 
+server.use(express.json())
+
+const projects: any = []
+
 server.get("/", (request, response) => {
     return response.send("Hello, welcome to my API REST!");
+});
+
+server.get("/projects", (request, response) => {
+    return response.json(projects)
+});
+
+server.post("/projects/", (request, response) => {
+    const { name, owner } = request.body
+    const project = {
+        id: uuidv4(),
+        name,
+        owner
+    }
+    projects.push(project)
+    return response.status(201).json(project)
 });
 
 export default server;
