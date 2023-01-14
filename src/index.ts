1554073e2c1340baa07bb3ec6d788297f554eeda
Aurélio Nogueira Mesquita <aurelio.mesquita@interaduaneira.com.br>
2023-01-14 18:09:38 -0300

    Adicionando estrutura de metodo delete

--- src/index.ts
+++ src/index.ts
@@ -40,5 +40,13 @@ server.put("/projects/:id", (request, response) => {
     projects[projectIndex] = project
     return response.json(project)
 })
+server.delete("/projects/:id", (request, response) => {
+    const { id } = request.params;
+    const projectIndex = projects.findIndex((p: any) => p.id === id);
+    if (projectIndex < 0) return response.status(404).json({ error: 'Project not found' });
+
+    projects.splice(projectIndex, 1)
+    return response.status(204).send()
+})
 
 export default server;
