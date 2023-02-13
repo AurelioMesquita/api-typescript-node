e8771163970e27cc4ceee8c2be31dbe80177d6ee
Aurélio Nogueira Mesquita <aurelio.mesquita@interaduaneira.com.br>
2023-02-13 20:29:10 -0300

    adicionando entities de roles

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,17 +1,18 @@
+import { Role } from "./entities/roles";
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
 
 const rolesRouter = Router();
 
-const roles = [];
+const roles: Role[] = [];
 
 rolesRouter.post("/", (res, response) => {
     const { name } = res.body;
-    const role = {
+
+    const role = new Role();
+    Object.assign(role, {
         name: name,
-        id: uuidv4(),
         created_at: new Date(),
-    };
+    });
 
     roles.push(role);
 
