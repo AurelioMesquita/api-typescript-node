c294df49bca39bee6619bc854f64ae85a74c8e11
Aurélio <aurelio.nogueira.mesquita@hotmail.com>
2023-04-16 15:30:14 -0300

    create avatar use case

---
+++ src/users/useCases/updateAvatar/updateAvatarUseCase.ts
@@ -0,0 +1,43 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "src/users/repositories/IUsersRepository";
+import { IRolesRepository } from "@roles/repositories/iRolesRepository";
+import { User } from "src/users/entities/user";
+import { AppError } from "@shared/errors/AppError";
+import { hash } from "bcryptjs";
+import path from "path";
+import uploadConfig from "@config/upload";
+import { fs } from "fs";
+export type UpdateAvatarDTO = {
+    userId: string;
+    avatarFileName: string;
+};
+
+@injectable()
+export class UpdateAvatarUseCase {
+    constructor(
+        @inject("UsersRepository") private UsersRepository: IUsersRepository,
+    ) { }
+    async execute({ userId, avatarFileName }: UpdateAvatarDTO): Promise<User> {
+        const user = await this.UsersRepository.findById(userId);
+
+        if (!user)
+            throw new AppError(
+                "Only authenticated users can change avatar.",
+                401,
+            );
+        if (user.avatar) {
+            const userAvatarFilePath = path.join(
+                uploadConfig.directory,
+                user.avatar,
+            );
+            const userAvatarFileExists = await fs.promise.stat(
+                userAvatarFilePath,
+            );
+            if (userAvatarFileExists)
+                await fs.promise.unlink(userAvatarFilePath);
+            user.avatar = avatarFileName;
+            return await this.UsersRepository.save(user);
+        }
+        return user;
+    }
+}
